42a813d2b23da9377116c23111cdefadcdf5c06b
pablo <paliutn@gmail.com>
2015-03-11 17:24:25 -0300

    hasta el 3 tuto de youtube

---
+++ listaDePersonas.js
@@ -0,0 +1,19 @@
+function ControladorPersonas($scope){
+
+	$scope.alumnos=
+	[
+
+     { nombre: 'pablo', apellido: 'Ocanto' ,domicilio: '45° 491', curso: 'Angular JS', fecha: new Date()}, 
+     
+     { nombre: 'Lucas', apellido: 'Jualisa' ,domicilio: '44° 712', curso: 'Diseño Web', fecha: new Date()},
+    
+     { nombre: 'Pedro', apellido: 'Chavez' ,domicilio:  '54° 213', curso: 'POO ', fecha: new Date()}, 
+
+     { nombre: 'David', apellido: 'Huilcañagui' ,domicilio: '80° 715', curso: 'Base e Datos', fecha: new Date()}
+
+
+     ];
+
+};
+
+	
---
+++ listadetareas.js
@@ -0,0 +1,34 @@
+function ControladorTareas($scope){
+
+	$scope.tareas=[{ texto:'Comprar milanesas', hecho:true }, { texto:'Aprender Angular Js', hecho:false }];
+
+	$scope.agregarTarea = function(){
+
+		$scope.tareas.push({texto:$scope.textoNuevaTarea,hecho:false});
+		$scope.textoNuevaTarea = '';
+
+	};
+
+	$scope.restantes = function (){
+
+		var cuenta =0;
+		angular.forEach($scope.tareas,function(tarea){
+            cuenta+=tarea.hecho ? 0 : 1;
+
+		});
+		return cuenta;
+	};
+
+   $scope.eliminar =function(){
+    
+    var tareasViejas = $scope.tareas;
+    $scope.tareas =[];
+    angular.forEach(tareasViejas,function(tarea){
+       if(!tarea.hecho){
+       	 $scope.tareas.push(tarea);
+       }
+      
+    });
+
+   }
+}
